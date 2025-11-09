b3f1a81bed6f0231987ea49f8a5bc834ebb95aea
Lakshitha <lakikissh@gamil.com>
2025-11-09 20:12:16 +0530

    fix: correct base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/aureostudio/", // 👈 Add this line (your repo name)
+  base: './', // 👈 Add this line (your repo name)
 })
